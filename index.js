8e23014444aad95245669a2337a8502b277e92f1
Ashish Kumar Patel <ashishkumarpatel2003@gmail.com>
2024-01-15 16:32:59 +0530

    get / console connection url

--- index.js
+++ index.js
@@ -13,12 +13,21 @@ dotenv.config();
 const app = express();
 app.use(cors())
 
+
+const DB_URL = process.env.CONNECTION_URL
+mongoose.connect(DB_URL, {}).then(()=>{
+    console.log("MongoDB database connected");
+}).catch((error)=>{
+    console.log(error)
+})
+
 app.use(express.json({limit:"30mb", extended:true}))
 app.use(express.urlencoded({limit:"30mb", extended:true}))
 app.use('/uploads',express.static(path.join('uploads')))
 
 app.get("/", (req, res) => {
     res.send("Hello");
+    res.send(process.env.CONNECTION_URL);
 });
 
 app.use(bodyParser.json())
@@ -31,11 +40,4 @@ app.use('/comment', commentRoutes)
 const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
     console.log(`Server Running on the PORT ${PORT}`)
-})
-
-const DB_URL = process.env.CONNECTION_URL
-mongoose.connect(DB_URL, {}).then(()=>{
-    console.log("MongoDB database connected");
-}).catch((error)=>{
-    console.log(error)
 })
