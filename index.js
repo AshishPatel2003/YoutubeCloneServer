083fb4b754a6832bea4317c598a5a948f00882e5
Ashish Kumar Patel <ashishkumarpatel2003@gmail.com>
2024-01-15 16:59:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({limit:"30mb", extended:true}))
 app.use('/uploads',express.static(path.join('uploads')))
 
 app.get("/", (req, res) => {
-    res.send("Hello", process.env.CONNECTION_URL, " Done");
+        res.send("Hello, "+process.env.CONNECTION_URL+", Done");
 });
 
 app.use(bodyParser.json())
